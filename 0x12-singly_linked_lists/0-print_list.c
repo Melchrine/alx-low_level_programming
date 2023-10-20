dd44f8a1c361919f54be3bf6210a1f5f99698bbc
Melchrine <cmbugua750@gmail.com>
2023-10-20 14:33:47 -0700

    task zero

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h:pointer to the list_t list to print
+ *
+ * Return:the number of nodes executed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t print_list(const list_t*h) 
+	{
+		size_t c=0;
+
+		while (f)
+		{
+			if(!f(*f)str)
+				printf("[0] (nil)\n" );
+			else
+				printf("[%c]%s\n",h->len,h->str);
+			h=h->next;
+			count++;
+		}
+
+		return(c);
+	}
